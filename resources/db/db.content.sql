b4253903de1230923876ced986d99b82c5a929db
Lisje <kleinewereld@gmail.com>
2013-08-12 11:10:35 +0200

    adding id's and missing third post

--- resources/db/db.content.sql
+++ resources/db/db.content.sql
@@ -1,15 +1,21 @@
-INSERT INTO post (title, slug, body) VALUES ('Time will tell', 'time-will-tell', 'Veggies sunt bona vobis, proinde vos postulo esse magis gourd watercress zucchini plantain bell pepper.
+﻿INSERT INTO post (id, title, slug, body) VALUES (1, 'Time will tell', 'time-will-tell', 'Veggies sunt bona vobis, proinde vos postulo esse magis gourd watercress zucchini plantain bell pepper.
 
 Chickweed grape spring onion spinach bok choy seakale sweet pepper zucchini rock melon salsify chard tomato daikon yarrow broccoli sierra leone bologi carrot kakadu plum. Chickweed courgette chickpea endive bitterleaf broccoli salad celtuce fava bean arugula tigernut watercress nori. Amaranth bunya nuts corn courgette spinach tomato wakame prairie turnip swiss chard lentil bush tomato eggplant burdock.
 
 Welsh onion water spinach asparagus fava bean daikon melon burdock. Gram cucumber beet greens sea lettuce seakale parsnip turnip rutabaga courgette bell pepper courgette bamboo shoot epazote garlic sorrel bunya nuts. Amaranth kale gumbo cress salad garlic chard summer purslane avocado green bean parsnip endive bok choy rutabaga leek cabbage pea sprouts ricebean. Pumpkin squash chickpea komatsuna kale arugula burdock salsify collard greens. Garbanzo catsear gumbo potato water chestnut sea lettuce welsh onion bush tomato bamboo shoot garlic mung bean');
 
-INSERT INTO post (title, slug, body) VALUES ('We are oceans apart', 'we-are-oceans-apart', 'Veggies sunt bona vobis, proinde vos postulo esse magis gourd watercress zucchini plantain bell pepper.
+INSERT INTO post (id, title, slug, body) VALUES (2, 'We are oceans apart', 'we-are-oceans-apart', 'Veggies sunt bona vobis, proinde vos postulo esse magis gourd watercress zucchini plantain bell pepper.
 
 Chickweed grape spring onion spinach bok choy seakale sweet pepper zucchini rock melon salsify chard tomato daikon yarrow broccoli sierra leone bologi carrot kakadu plum. Chickweed courgette chickpea endive bitterleaf broccoli salad celtuce fava bean arugula tigernut watercress nori. Amaranth bunya nuts corn courgette spinach tomato wakame prairie turnip swiss chard lentil bush tomato eggplant burdock.
 
 Welsh onion water spinach asparagus fava bean daikon melon burdock. Gram cucumber beet greens sea lettuce seakale parsnip turnip rutabaga courgette bell pepper courgette bamboo shoot epazote garlic sorrel bunya nuts. Amaranth kale gumbo cress salad garlic chard summer purslane avocado green bean parsnip endive bok choy rutabaga leek cabbage pea sprouts ricebean. Pumpkin squash chickpea komatsuna kale arugula burdock salsify collard greens. Garbanzo catsear gumbo potato water chestnut sea lettuce welsh onion bush tomato bamboo shoot garlic mung bean');
 
-INSERT INTO tag (label, slug) VALUES ('joske', 'joske'), ('deep thoughts', 'deep-thoughts'), ('all things green', 'all-things-green');
+INSERT INTO post (id, title, slug, body) VALUES (3, 'We are oceans apart again', 'we-are-oceans-apart-again', 'Veggies sunt bona vobis, proinde vos postulo esse magis gourd watercress zucchini plantain bell pepper.
+
+Chickweed grape spring onion spinach bok choy seakale sweet pepper zucchini rock melon salsify chard tomato daikon yarrow broccoli sierra leone bologi carrot kakadu plum. Chickweed courgette chickpea endive bitterleaf broccoli salad celtuce fava bean arugula tigernut watercress nori. Amaranth bunya nuts corn courgette spinach tomato wakame prairie turnip swiss chard lentil bush tomato eggplant burdock.
+
+Welsh onion water spinach asparagus fava bean daikon melon burdock. Gram cucumber beet greens sea lettuce seakale parsnip turnip rutabaga courgette bell pepper courgette bamboo shoot epazote garlic sorrel bunya nuts. Amaranth kale gumbo cress salad garlic chard summer purslane avocado green bean parsnip endive bok choy rutabaga leek cabbage pea sprouts ricebean. Pumpkin squash chickpea komatsuna kale arugula burdock salsify collard greens. Garbanzo catsear gumbo potato water chestnut sea lettuce welsh onion bush tomato bamboo shoot garlic mung bean');
+
+INSERT INTO tag (id, label, slug) VALUES (1, 'joske', 'joske'), (2, 'deep thoughts', 'deep-thoughts'), (3, 'all things green', 'all-things-green');
 
 INSERT INTO post_tag (post_id, tag_id) VALUES (1, 1), (1, 3), (2, 2), (3, 3);
